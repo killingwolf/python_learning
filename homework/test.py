1af1927204899a988629ba4319f918dd7768a610
killingwolf <killingwolf@qq.com>
2015-11-16 00:08:26 +0800

    2-16,17,23 verion 2

--- homework/test.py
+++ homework/test.py
@@ -4,31 +4,5 @@
 # Email: killingwolf@qq.com
 "This is a hello world module"
 
-import time
-
-
-def time_me(fn):
-    def _wrapper(*args, **kwargs):
-        start = time.clock()
-        fn(*args, **kwargs)
-        print "%s cost %s second" % (fn.__name__, time.clock() - start)
-    return _wrapper
-
-
-@time_me
-def time_list(data):
-    return len([word for line in data for word in line.split()])
-
-
-@time_me
-def time_gen(data):
-    return len([word for line in data for word in line.split()])
-
-
-if __name__ == '__main__':
-    ff = r'C:/Users/zhanghengfeng/Desktop/messages'
-    data = open(ff, 'r')
-    print time_list(data)
-    data = open(ff, 'r')
-    ff = r'C:/Users/zhanghengfeng/Desktop/messages'
-    print time_gen(data)
+print "#" * 80
+print "######################################################d################"
