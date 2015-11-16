ca9462cac5e2194c67d13cef65a4c0c42fcdf984
killingwolf <killingwolf@qq.com>
2015-11-16 10:35:27 +0800

    giant commit

--- homework/test.py
+++ homework/test.py
@@ -2,33 +2,14 @@
 # -*- coding: utf-8 -*-
 # Author: killingwolf
 # Email: killingwolf@qq.com
-"This is a hello world module"
+""" This is a hello world module
+    dfadfadfa
+"""
 
-import time
-
-
-def time_me(fn):
-    def _wrapper(*args, **kwargs):
-        start = time.clock()
-        fn(*args, **kwargs)
-        print "%s cost %s second" % (fn.__name__, time.clock() - start)
-    return _wrapper
-
-
-@time_me
-def time_list(data):
-    return len([word for line in data for word in line.split()])
-
-
-@time_me
-def time_gen(data):
-    return len([word for line in data for word in line.split()])
-
+def main():
+    print "#" * 80
+    if 'xxxx' == 'xxxxx':
+        pass
 
 if __name__ == '__main__':
-    ff = r'C:/Users/zhanghengfeng/Desktop/messages'
-    data = open(ff, 'r')
-    print time_list(data)
-    data = open(ff, 'r')
-    ff = r'C:/Users/zhanghengfeng/Desktop/messages'
-    print time_gen(data)
+    main()
---
+++ video_code/insert.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#coding:utf-8
+"""
+求阶乘和
+"""
+
+def jiecheng(n):
+    """q
+    """
